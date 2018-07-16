45cffff841c853c2098620aa734054f8fd31dbd5
Tiffany Ma <tma@fb.com>
2018-07-16 14:48:49 -0700

    Added Forecast.io API connection with a tester button

---
+++ API/APIManager.m
@@ -0,0 +1,60 @@
+//
+//  APIManager.m
+//  WeatherApp
+//
+//  Created by Tiffany Ma on 7/16/18.
+//  Copyright © 2018 xToBeAnnounce. All rights reserved.
+//
+
+#import "APIManager.h"
+
+static NSString * const baseURLString = @"https://api.darksky.net/forecast/";
+static NSString * const consumerSecret = @"41cbd5a478f0e0165572447fdb67d4db/";
+static NSString * urlString;
+
+@implementation APIManager
+
++ (instancetype)shared {
+    static APIManager *sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedManager = [[self alloc] init];
+    });
+    return sharedManager;
+}
+
+- (instancetype)init{
+    urlString = [baseURLString stringByAppendingString:consumerSecret];
+    return self;
+}
+
+- (void)getDataWithLatitude:(int)lat Longitude:(int)lng WithCompletion:(void(^)(NSDictionary *data, NSError *error))completion{
+    NSString *coordinateStr = [NSString stringWithFormat:@"%d,%d", lat, lng];
+    
+    urlString = [urlString stringByAppendingString:coordinateStr];
+    
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSLog(@"%@", url);
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+        NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            NSLog(@"%@", error);
+            if (error != nil) {
+                NSLog(@"ERROR");
+                completion(nil, error);
+            }
+            else {
+                NSLog(@"Worked");
+                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+                NSLog(@"%@", dataDictionary);
+                completion(dataDictionary, nil);
+            }
+        }];
+        [task resume];
+    });
+}
+
+@end
--- WeatherApp/ViewController.m
+++ WeatherApp/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "APIManager.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button addTarget:self action:@selector(onTapTestAPI) forControlEvents:UIControlEventTouchUpInside];
+    [button setTitle:@"Test API" forState:UIControlStateNormal];
+    button.frame = CGRectMake(80.0, 210.0, 160.0, 40.0);
+    [button setBackgroundColor:[UIColor redColor]];
+    [self.view addSubview:button];
+}
+
+- (void)onTapTestAPI{
+    [[APIManager shared] getDataWithLatitude:42.3601 Longitude: -71.0589 WithCompletion:^(NSDictionary *data, NSError *error) {
+        if(error != nil){
+            NSLog(@"%@", data);
+        }
+        else{
+            NSLog(@"FAILED");
+        }
+    }];
 }
 
 
