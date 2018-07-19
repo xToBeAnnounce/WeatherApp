3ba4c73e9ffb60ab4140fd3398988b1d9aa8d129
Tiffany Ma <tma@fb.com>
2018-07-19 16:47:21 -0700

    Weather Model created with data displaying, fixing cell-overlapping issue

---
+++ WeatherApp/Models/Weather.h
@@ -0,0 +1,13 @@
+//
+//  Weather.h
+//  WeatherApp
+//
+//  Created by Tiffany Ma on 7/19/18.
+//  Copyright © 2018 xToBeAnnounce. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Weather : NSObject
+
+@end
---
+++ WeatherApp/Models/Weather.m
@@ -0,0 +1,13 @@
+//
+//  Weather.m
+//  WeatherApp
+//
+//  Created by Tiffany Ma on 7/19/18.
+//  Copyright © 2018 xToBeAnnounce. All rights reserved.
+//
+
+#import "Weather.h"
+
+@implementation Weather
+
+@end
