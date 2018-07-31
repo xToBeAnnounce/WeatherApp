ef41a4e3882f7c4da79557738e034785565e002d
Tiffany <brownie.tm@gmail.com>
2018-07-31 14:35:46 -0700

    Took out logout at appInit

--- WeatherApp/AppDelegate.m
+++ WeatherApp/AppDelegate.m
@@ -25,7 +25,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [self parseBackendSetup];
-    [User logOut];
     self.mainNavController = [[NavigationController alloc] init];
     
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
