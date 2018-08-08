6e268c9a83bab269360e9e12407a644bce41df9f
Jamie Tan <jamietan@fb.com>
2018-08-08 12:21:12 -0700

    Fixed another merge conflict

--- WeatherApp/NewUIViews/TodayWeatherCell.h
+++ WeatherApp/NewUIViews/TodayWeatherCell.h
@@ -7,18 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
-<<<<<<< HEAD
 #import "TodayWeatherView.h"
+
 @interface TodayWeatherCell : UITableViewCell
 @property (strong, nonatomic) TodayWeatherView *weatherView;
-=======
-#import "Weather.h"
-
-@interface TodayWeatherCell : UITableViewCell
-
-@property (strong, nonatomic) NSString *tempTypeString;
-@property (strong, nonatomic) Weather *currentWeather;
-@property (strong, nonatomic) Weather *todayWeather;
-
->>>>>>> 5f4f0fc1d1352127dd9b65484fe75e94b925804c
 @end
